6b9d0f2a1909509d2b8d98a4d685fc1e8e697863
victor-alexandre <victor.fisica01@gmail.com>
2019-05-29 11:57:57 -0300

    Adicionei a vizualização de imagens e upload de imagens para a cloud por usuário.

--- app/src/main/java/com/example/loginv3/ProfileActivity.java
+++ app/src/main/java/com/example/loginv3/ProfileActivity.java
@@ -1,42 +1,75 @@
 package com.example.loginv3;
 
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.graphics.BitmapFactory;
+
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnPausedListener;
+import com.google.firebase.storage.OnProgressListener;
+
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
 
+
+
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "ProfileActivity";
+
+    private static final int SELECT_PICTURE = 100;
+    private static final int UPLOAD_PICTURE = 200;
+
+
     private TextView mTextView;
     private EditText usernameField;
     private FirebaseAuth mAuth;
     private DatabaseReference mDatabase;
     private ImageView mImage;
+    private StorageReference storageRef;
+    private FirebaseStorage storage;
 
+    private FirebaseUser mUser;
+
+    private static Context mContext;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,7 +77,9 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
         findViewById(R.id.updateButton).setOnClickListener(this);
         findViewById(R.id.logoutButton).setOnClickListener(this);
+        findViewById(R.id.atualizarButton).setOnClickListener(this);
         findViewById(R.id.profileImage).setOnClickListener(this);
+
 
         mTextView = findViewById(R.id.welcomeText);
         usernameField = findViewById(R.id.usernameField);
@@ -52,6 +87,7 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
         //autenticação
         mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
 
         mTextView.setText("Seja bem vindo de volta " + mAuth.getCurrentUser().getEmail());
 
@@ -61,12 +97,15 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
 
         //Cloud firestorage
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference storageRef = storage.getReference();
-        StorageReference imagesRef = storageRef.child("images");
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
 
+        mContext = getApplicationContext();
 
+        //checkFilePermissions();
+        handlePermission();
         updateDisplayName();
+        updateProfilePicture();
     }
 
     @Override
@@ -80,9 +119,16 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
             signOut();
         }
         if (i == R.id.profileImage){
-            //uploadImg();
-            pickFromGallery();
+            openImageChooser(UPLOAD_PICTURE);
         }
+
+        if(i == R.id.atualizarButton){
+            refreshScreen();
+        }
+    }
+
+    private void refreshScreen(){
+        updateProfilePicture();
     }
 
     private void signOut() {
@@ -93,6 +139,16 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     private void updateUserData(String userId, String name) {
         mDatabase.child("users").child(userId).child("username").setValue(name);
         updateDisplayName();
+        updateProfilePicture();
+
+    }
+
+
+    private void updateProfilePicture(){
+        StorageReference pathReference = storageRef.child("images/"+mUser.getUid()+"/profilePic");
+        Glide.with(mContext)
+                .load(pathReference)
+                .into(mImage);
     }
 
     private void updateDisplayName(){
@@ -113,6 +169,348 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     }
 
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void checkFilePermissions() {
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP){
+            int permissionCheck = ProfileActivity.this.checkSelfPermission("Manifest.permission.READ_EXTERNAL_STORAGE");
+            permissionCheck += ProfileActivity.this.checkSelfPermission("Manifest.permission.WRITE_EXTERNAL_STORAGE");
+            if (permissionCheck != 0) {
+                this.requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE,android.Manifest.permission.READ_EXTERNAL_STORAGE}, 1001); //Any number
+            }
+        }else{
+            Log.d(TAG, "checkBTPermissions: No need to check permissions. SDK version < LOLLIPOP.");
+        }
+    }
+
+////////////////////////////////////inicio do código que funciona
+
+
+
+    private void handlePermission() {
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return;
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            //ask for permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    SELECT_PICTURE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case SELECT_PICTURE:
+                for (int i = 0; i < permissions.length; i++) {
+                    String permission = permissions[i];
+                    if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                        boolean showRationale = ActivityCompat.shouldShowRequestPermissionRationale(this, permission);
+                        if (showRationale) {
+                            //  Show your own message here
+                        } else {
+                            showSettingsAlert();
+                        }
+                    }
+                }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
+
+
+
+
+
+
+
+   // private static final String TAG = "SelectImageActivity";
+    /* Choose an image from Gallery */
+    void openImageChooser(int requestCode) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), requestCode);
+    }
+
+    public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (resultCode == RESULT_OK) {
+                    if (requestCode == SELECT_PICTURE) {
+                        // Get the url from data
+                        final Uri selectedImageUri = data.getData();
+                        if (null != selectedImageUri) {
+                            // Get the path from the Uri
+                            String path = getPathFromURI(selectedImageUri);
+                            Log.i(TAG, "Image Path : " + path);
+                            // Set the image in ImageView
+                            findViewById(R.id.profileImage).post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    ((ImageView) findViewById(R.id.profileImage)).setImageURI(selectedImageUri);
+                                }
+                            });
+
+                        }
+
+                    }
+                    if(requestCode == UPLOAD_PICTURE){
+                        // Get the url from data
+                        final Uri selectedImageUri = data.getData();
+                        if (null != selectedImageUri) {
+                            // Get the path from the Uri
+                            String path = getRealPathFromURI_API19(mContext, selectedImageUri);
+                            Log.i(TAG, "Image Path : " + path);
+                            Log.i(TAG, "Image real path : " + String.valueOf(selectedImageUri));
+
+                            //Codar aqui o código para enviar para a CLOUD
+
+                            // File or Blob
+                            String manualPath = "/storage/emulated/0/Download/aa.jpeg";
+
+                            //String realPath = Context.getFilesDir();
+
+                            Uri file = Uri.fromFile(new File(path));
+
+                            // Create the file metadata
+                           /* metadata = new StorageMetadata.Builder()
+                                    .setContentType("image/jpeg")
+                                    .build();
+                            */
+
+                            // Upload file and metadata to the path 'images/mountains.jpg'
+                             UploadTask uploadTask = storageRef.child("images/" + mUser.getUid()+ "/profilePic").putFile(file);
+
+                            // Listen for state changes, errors, and completion of the upload.
+                            uploadTask.addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                                @Override
+                                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                                    double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                                    System.out.println("Upload is " + progress + "% done");
+                                }
+                            }).addOnPausedListener(new OnPausedListener<UploadTask.TaskSnapshot>() {
+                                @Override
+                                public void onPaused(UploadTask.TaskSnapshot taskSnapshot) {
+                                    System.out.println("Upload is paused");
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception exception) {
+                                    // Handle unsuccessful uploads
+                                }
+                            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                @Override
+                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                    // Handle successful uploads on complete
+                                    // ...
+                                }
+                            });
+
+
+                            // Set the image in ImageView
+                            findViewById(R.id.profileImage).post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    ((ImageView) findViewById(R.id.profileImage)).setImageURI(selectedImageUri);
+                                }
+                            });
+
+                        }
+                    }
+                }
+
+            }
+        }).start();
+
+    }
+
+    @SuppressLint("NewApi")
+    public static String getRealPathFromURI_API19(Context context, Uri uri){
+        String filePath = "";
+        String wholeID = DocumentsContract.getDocumentId(uri);
+
+        // Split at colon, use second item in the array
+        String id = wholeID.split(":")[1];
+
+        String[] column = { MediaStore.Images.Media.DATA };
+
+        // where id is equal to
+        String sel = MediaStore.Images.Media._ID + "=?";
+
+        Cursor cursor = context.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                column, sel, new String[]{ id }, null);
+
+        int columnIndex = cursor.getColumnIndex(column[0]);
+
+        if (cursor.moveToFirst()) {
+            filePath = cursor.getString(columnIndex);
+        }
+        cursor.close();
+        return filePath;
+    }
+
+
+    private String getRealPathFromURI(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } catch (Exception e) {
+            Log.e(TAG, "getRealPathFromURI Exception : " + e.toString());
+            return "";
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
+
+    /* Get the real path from the URI */
+    public String getPathFromURI(Uri contentUri) {
+        String res = null;
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
+        if (cursor.moveToFirst()) {
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            res = cursor.getString(column_index);
+        }
+        cursor.close();
+        return res;
+    }
+
+
+    private void showSettingsAlert() {
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle("Alert");
+        alertDialog.setMessage("App needs to access the Camera.");
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "DONT ALLOW",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        //finish();
+                    }
+                });
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "SETTINGS",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        openAppSettings(ProfileActivity.this);
+                    }
+                });
+        alertDialog.show();
+    }
+
+    public static void openAppSettings(final Activity context) {
+        if (context == null) {
+            return;
+        }
+        final Intent i = new Intent();
+        i.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        i.addCategory(Intent.CATEGORY_DEFAULT);
+        i.setData(Uri.parse("package:" + context.getPackageName()));
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+        context.startActivity(i);
+    }
+
+
+//////////////////////////////////////////Fim do codigo que funciona
+
+/*
+private void uploadIMG(){
+    //get the signed in user
+    FirebaseUser user = mAuth.getCurrentUser();
+    String userID = user.getUid();
+        Uri uri = Uri.fromFile(new File(pathArray.get(array_position)));
+        StorageReference storageReference = mStorageRef.child("images/users/" + userID + "/" + name + ".jpg");
+        storageReference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // Get a URL to the uploaded content
+                Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                toastMessage("Upload Success");
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                toastMessage("Upload Failed");
+
+            }
+        })
+        ;
+
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    private void tentativaUpload (){
+        // File or Blob
+        File file = Uri.fromFile(new File("path/to/mountains.jpg"));
+
+
+
+        // Create the file metadata
+        metadata = new StorageMetadata.Builder()
+                .setContentType("image/jpeg")
+                .build();
+
+        // Upload file and metadata to the path 'images/mountains.jpg'
+        uploadTask = storageRef.child("images/"+file.getLastPathSegment()).putFile(file, metadata);
+
+        // Listen for state changes, errors, and completion of the upload.
+        uploadTask.addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                System.out.println("Upload is " + progress + "% done");
+            }
+        }).addOnPausedListener(new OnPausedListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onPaused(UploadTask.TaskSnapshot taskSnapshot) {
+                System.out.println("Upload is paused");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // Handle successful uploads on complete
+                // ...
+            }
+        });
+    }
+*/
     //Tentativa 2 de upar imagens- deu mais ou menos certo
 
 
@@ -210,5 +608,9 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
 
 }
+
+
+
+
 
 
